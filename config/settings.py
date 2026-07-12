4081a70ddc07befafedc560b875c91d2071ad2b8
amantiwari79 <ak1309581@gmail.com>
2026-07-12 10:45:03 +0530

    urls change

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-local-dev-secret-key-
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='two026-3as1.onrender.com').split(',')
 
 
 # Application definition
