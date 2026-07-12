2f6fc60833a0f6dfa0d85035e1a5567a1bb41c20
amantiwari79 <ak1309581@gmail.com>
2026-07-12 11:56:12 +0530

    Fix media url serving path and change pdf preview tag from embed to iframe

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media Files (Resume Uploads)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Authentication URLs
